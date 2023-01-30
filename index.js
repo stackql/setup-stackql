0b16299e9423c16674fbfb1e269354fcc40b8e1b
jeffreyaven <javen@avensolutions.com>
2023-01-30 16:29:23 +1100

    platform testing

--- index.js
+++ index.js
@@ -1,50 +1,70 @@
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-
-// External
 const io = require('@actions/io');
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 const { execSync } = require("child_process");
 
-
 const urls = {
-    'ubuntu': 'https://releases.stackql.io/stackql/latest/stackql_linux_amd64.zip',
+  'linux': 'https://releases.stackql.io/stackql/latest/stackql_linux_amd64.zip',
+  'darwin': 'https://storage.googleapis.com/stackql-public-releases/latest/stackql_darwin_multiarch.pkg',
+  'win32': 'https://releases.stackql.io/stackql/latest/stackql_windows_amd64.zip',
 }
 
-async function downloadCLI(){
-    try {
-        const url = urls['ubuntu']
-        const pathToCLIZip = await tc.downloadTool(url);
-
-        let pathToCLI = '';
-  
-        pathToCLI = await tc.extractZip(pathToCLIZip);
-
-        fs.chmodSync(pathToCLI, '777')
-
-        core.info(`Stackql CLI path is ${pathToCLI}.`);
-
-        return pathToCLI      
-    
-    } catch (error) {
-        core.error(error);
-        throw error;
-      }
-}
-
-async function addPermission(){
+async function downloadCLI(osPlatform){
   try {
-    execSync("find ~ -name stackql -exec chmod +x {} \\;");
-    console.log("Successfully gave execute permission to stackql");
+
+    core.info(`downloading stackql binary for ${osPlatform}`);
+    const url = urls[osPlatform]
+    core.debug(`binary location: ${url}`);
+
+    let pathToCLI = '';
+
+    switch (osPlatform) {
+      case 'win32':
+        const pathToWinZip = await tc.downloadTool(url);
+        pathToCLI = await tc.extractZip(pathToWinZip);
+        core.debug(`path to cli: ${pathToCLI}`);
+        fs.chmodSync(pathToCLI, '777');
+        return pathToCLI;
+      case 'darwin':
+        const pathToMacPkg = await tc.downloadTool(url);
+        pathToCLI = await tc.extractXar(pathToMacPkg);
+        core.debug(`path to cli: ${pathToCLI}`);
+        fs.chmodSync(pathToCLI, '777');
+        return pathToCLI;
+      case 'linux':
+        const pathToLinuxZip = await tc.downloadTool(url);
+        pathToCLI = await tc.extractZip(pathToLinuxZip);
+        core.debug(`path to cli: ${pathToCLI}`);
+        fs.chmodSync(pathToCLI, '777');
+        return pathToCLI;
+      default:
+        throw new Error(`Unsupported platform: ${osPlatform}`);
+    }
+
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    core.error(error);
+    throw error;
   }
 }
 
+async function makeExecutable(osPlatform){
+  try {
+    core.debug(`making ${pathToCLI} executable...`);
+    if(osPlatform === 'win32'){
+      execSync("Get-ChildItem ~ -Filter 'stackql' | ForEach-Object { Set-ExecutionPolicy Unrestricted -Scope Process; $.FullName } | ForEach-Object { Set-ItemProperty $.FullName -Name IsReadOnly -Value $False } | ForEach-Object { & $_.FullName }");
+    } else {
+      execSync("find ~ -name stackql -exec chmod +x {} \\;");
+    }
+    core.debug(`successfully made ${pathToCLI} executable`);
+  } catch (error) {
+    core.error(`Error: ${error.message}`);
+  }
+}
 
-async function installWrapper (pathToCLI) {
+async function installWrapper(pathToCLI) {
   let source, target;
 
   // If we're on Windows, then the executable ends with .exe
@@ -81,21 +101,19 @@ async function setup(){
 
   const osPlatform = os.platform();
   const osArch = os.arch();
-  console.log(`Platform: ${osPlatform}`);
-  console.log(`Arch: ${osArch}`);
+  core.debug(`platform: ${osPlatform}`);
+  core.debug(`arch: ${osArch}`);
 
-  // Platform: linux
-  // Arch: x64
-
-  const path = await downloadCLI()
+  const path = await downloadCLI(osPlatform)
 
   core.addPath(path)
-  await addPermission()
+  await makeExecutable(osPlatform)
   const wrapper = core.getInput('use_wrapper') === 'true';
   if(wrapper){
-    console.log('installing wrapper')
+    core.debug('installing wrapper')
     await installWrapper(path)
   }
+  core.info(`successfully setup stackql at ${path}`);
 }
 
 (async () => {
